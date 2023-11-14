5be3accd9c28b99ddc540d01669e5ec203e28394
Tommy Ly <lytommy321@gmail.com>
2023-11-14 11:54:44 -0800

    added comments to understand the code

--- test1.py
+++ test1.py
@@ -1,26 +1,26 @@
 #I will begin working on the project
-import sys, pygame
-pygame.init()
+import sys, pygame    #import libraries
+pygame.init()           #initialize the library
 
-size = width, height = 320, 240
-speed = [2, 2]
-black = 0, 0, 0
+size = width, height = 320, 240    #define the game window size
+speed = [2, 2]                      #define The speed of how fast the ball will bounce
+black = 0, 0, 0                     #define the color of the screen background
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size)  #setup a screen
 
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+ball = pygame.image.load("intro_ball.gif")      #create a ball object
+ballrect = ball.get_rect()                      #create a rectangle for hitbox detection for the ball.and apply to the ball object
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+while True:                                     #game loop
+    for event in pygame.event.get():            #Waiting for an event like input
+        if event.type == pygame.QUIT: sys.exit()        #if the event we receive is quit then exit the game.
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
+    ballrect = ballrect.move(speed)             #The rectangle of the ball moves at the given speed.
+    if ballrect.left < 0 or ballrect.right > width:     #if cant go further left then travel right and vice versa
         speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
+    if ballrect.top < 0 or ballrect.bottom > height:       #if cant go further down, then travel up and vice versa.
         speed[1] = -speed[1]
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+    screen.fill(black)                          #background screen color
+    screen.blit(ball, ballrect)                 #idk what blit means
+    pygame.display.flip()                       #idk what flip means
