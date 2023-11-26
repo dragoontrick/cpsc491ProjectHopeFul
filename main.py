9d93b067e75561b301a5e39b6d497c03c15aa96f
Tommy Ly <lytommy321@gmail.com>
2023-11-25 21:18:59 -0800

    ready to place alien group

--- main.py
+++ main.py
@@ -14,16 +14,22 @@ class Game:
         self.shape = obstacle.shape
         self.block_size = 6
         self.blocks = pygame.sprite.Group()
-        self.create_obstacle()
+        self.obstacle_amount = 4
+        self.obstacle_x_positions = [num *(screen_width/self.obstacle_amount) for num in range(self.obstacle_amount)] #0,1,2,3 and gets a position 
+        self.create_multiple_obstacles(*self.obstacle_x_positions,x_start= screen_width/15, y_start= 380)  #values 0,100,200 are all part of offset tuple *unpacks
 
-    def create_obstacle(self):
+    def create_obstacle(self, x_start, y_start, offset_x):
         for row_index, row in enumerate(self.shape):
             for col_index, col in enumerate(row):
                 if col =='x':
-                    x = col_index * self.block_size
-                    y = row_index * self.block_size
+                    x = x_start + col_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
                     block = obstacle.Block(self.block_size,(241,79,80),x,y)
                     self.blocks.add(block)
+
+    def create_multiple_obstacles(self,*offset, x_start,y_start):
+        for offset_x in offset:
+            self.create_obstacle(x_start,y_start,offset_x)
 
     def run(self):
         self.player.sprite.lasers.draw(screen)
